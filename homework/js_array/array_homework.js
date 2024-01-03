f5c91b6d1cf3c6d7fa4fee94ce32032188a231a1
Adina <adinaa_lavinia@yahoo.com>
2024-01-03 12:25:09 +0200

    js array

--- homework/js_array/array_homework.js
+++ homework/js_array/array_homework.js
@@ -3,7 +3,7 @@
 // 1.Declarati si initializati un alt array denumit otherValues si populatil la fel ca initialValues dar cu mai multe tipuri de date dar. Trebuie sa contina macar un tip numeric si un string;
 // 2.Faceti push la otherValues array in initialValues array;
 // 3.Folositi valorile de tip numeric din initialValues pentru a calcula cateva operatii aritmetice(ex. Puteti adauga 2 la o anumita valoare);
-// 4.Dupa ce a-ti facut operatiile, modificati valorile din initialValues array cu noile valori obtinute pe baza operatiilor. Valorile sa fie puse pe pozitiile corespunzatoare, sa nu fie adaugate ca valori noi in array.
+// 4.Dupa ce ati facut operatiile, modificati valorile din initialValues array cu noile valori obtinute pe baza operatiilor. Valorile sa fie puse pe pozitiile corespunzatoare, sa nu fie adaugate ca valori noi in array.
 // Ex: Input: [1, 2, 3]
 //       Output: [3, 4, 5] // Am adunat 2 la fiecare numar din array
 // 5.Concatenati toate valorile de tip string din initialValues array cu string-ul ”happy codding”;
@@ -14,7 +14,7 @@
 
 
 
-let initialValues = [true, 1, 3, "string", undefined, null];
+let initialValues = [true, false, 1, 3, 6, 788, "mama", "tata", undefined, null];
 
 // 1.
 let otherValues = [undefined, null, 467, 345, 567, "asd", "asdfg", false];
@@ -22,4 +22,29 @@ let otherValues = [undefined, null, 467, 345, 567, "asd", "asdfg", false];
 // 2. 
 initialValues.push(otherValues);
 console.log(initialValues);
+// let newArray = initialValues.concat(otherValues);
+// console.log(newArray);
 
+// 3. 4. 
+for(let i = 0; i < initialValues.length; i++){
+    if(typeof initialValues[i] === "number"){
+        initialValues[i] = initialValues[i] + 10;
+    }
+}
+console.log(initialValues);
+
+// 5. 
+for(let i = 0; i < initialValues.length; i++){
+    if(typeof initialValues[i] === "string"){
+        initialValues[i] += "happy codding";
+    }
+}
+console.log(initialValues);
+
+// 6. 
+for(let i = 0; i < initialValues.length; i++){
+    if(typeof initialValues[i] === "boolean"){
+        initialValues[i] = !initialValues[i];
+    }
+}
+console.log(initialValues);
