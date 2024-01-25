a28d711722e0f1a083f1de2108ab76d5df69ad3b
Adina <adinaa_lavinia@yahoo.com>
2024-01-25 14:02:59 +0200

    dom

---
+++ homework/js_dom_manipulation/tabla-sah.js
@@ -0,0 +1,83 @@
+for(let i = 1; i < 9; i++){
+    let box1 = document.createElement('div');
+    if(i % 2 == 0){
+        box1.style.backgroundColor = 'white';
+    } else {
+        box1.style.backgroundColor = 'black';
+    }
+    table.appendChild(box1);
+}
+// box1
+
+for(let i = 1; i < 9; i++){
+    let box2 = document.createElement('div');
+    if(i % 2 == 0){
+        box2.style.backgroundColor = 'black';
+    } else {
+        box2.style.backgroundColor = 'white';
+    }
+    table.appendChild(box2);
+}
+
+for(let i = 1; i < 9; i++){
+    let box3 = document.createElement('div');
+    if(i % 2 == 0){
+        box3.style.backgroundColor = 'white';
+    } else {
+        box3.style.backgroundColor = 'black';
+    }
+    table.appendChild(box3);
+}
+
+for(let i = 1; i < 9; i++){
+    let box4 = document.createElement('div');
+    if(i % 2 == 0){
+        box4.style.backgroundColor = 'black';
+    } else {
+        box4.style.backgroundColor = 'white';
+    }
+    table.appendChild(box4);
+}
+
+for(let i = 1; i < 9; i++){
+    let box5 = document.createElement('div');
+    if(i % 2 == 0){
+        box5.style.backgroundColor = 'white';
+    } else {
+        box5.style.backgroundColor = 'black';
+    }
+    table.appendChild(box5);
+}
+
+for(let i = 1; i < 9; i++){
+    let box6 = document.createElement('div');
+    if(i % 2 == 0){
+        box6.style.backgroundColor = 'black';
+    } else {
+        box6.style.backgroundColor = 'white';
+    }
+    table.appendChild(box6);
+}
+
+for(let i = 1; i < 9; i++){
+    let box7 = document.createElement('div');
+    if(i % 2 == 0){
+        box7.style.backgroundColor = 'white';
+    } else {
+        box7.style.backgroundColor = 'black';
+    }
+    table.appendChild(box7);
+}
+
+for(let i = 1; i < 9; i++){
+    let box8 = document.createElement('div');
+    if(i % 2 == 0){
+        box8.style.backgroundColor = 'black';
+    } else {
+        box8.style.backgroundColor = 'white';
+    }
+    table.appendChild(box8);
+}
+
+
+
