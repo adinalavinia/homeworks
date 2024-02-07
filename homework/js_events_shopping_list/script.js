35015d6047a5fceff5ca4f5f35c619489359cd74
Adina <adinaa_lavinia@yahoo.com>
2024-02-07 21:27:54 +0200

    shopping list

--- homework/js_events_shopping_list/script.js
+++ homework/js_events_shopping_list/script.js
@@ -1,64 +1,38 @@
 const button = document.querySelector('.button');
 const itemsList = document.querySelector('.items-list');
-button.addEventListener('click', () => {
-    let element = document.createElement('p');
-    element.innerHTML = document.getElementById('textItem').value;
-    itemsList.appendChild(element);
-    
 
-    // let check = document.createElement('input');
-    // check.setAttribute('type', 'checkbox');
-    // console.log(check);
+button.addEventListener('click', createElementList);
 
-    // check.innerHTML = document.getElementById('textItem').value;
-
-    // check.appendChild(element);
-
-    // let deleteBtn = document.createElement('span');
-    // deleteBtn.innerHTML = document.getElementsByTagName('p');
-    // itemsList.appendChild(deleteBtn);
-
-    
+function createElementList() {
 
 
-
-
-
+    let li = document.createElement('li');
+    li.innerHTML = document.getElementById('textItem').value;
     document.getElementById('textItem').value = '';
+
+    let input = document.createElement('input');
+    input.type = 'checkbox';
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerHTML = '<span class="material-symbols-outlined">close</span>';
+    deleteBtn.style.backgroundColor = 'transparent';
+    deleteBtn.style.border = 'none';
+    deleteBtn.style.cursor = 'pointer';
+
+    li.appendChild(input);
+    li.appendChild(deleteBtn);
+    itemsList.appendChild(li);
     
-    element.addEventListener('click', () => {
-        element.style.textDecoration = 'line-through';
-    })
-
-    // let span = document.getElementById('span');
-    // if(element.style.display = 'block'){
-    //     span.style.display = 'block';
-    // }
-
-
-    // if(itemsList.value == 10){
-    //     button.disable = true;
-    // }
     
-    let span = document.getElementById('span');
-    for(let i = 0; i < itemsList.length; i++){
-        itemsList[i] + span.style.display;
-
-        // if(itemsList[i].style.display = 'block'){
-        //     span.style.display = 'block';
-        //     span.style.position = 'absolute';
-        // }
-
+    deleteBtn.addEventListener('click', removeItem);
+    
+    function removeItem(e){
+        const item = e.target.value;
+        li.remove(item);
     }
+      
+}
 
-})
 
 
 
-// for(let i = 0; i < itemsList.length; i++){
-//     let span = document.createElement('SPAN');
-//     let txt = document.createTextNode('\u00D7');
-//     span.className = 'close';
-//     span.appendChild(txt);
-//     itemsList[i].appendChild(span);
-// }
